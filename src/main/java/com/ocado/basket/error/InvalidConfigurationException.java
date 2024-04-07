9bf52ba16ea9cabd9cef046e918f07b4a4f123a9
Mateusz <matgalv2@gmail.com>
2024-04-07 18:48:28 +0200

    Added error for invalid configuration

---
+++ src/main/java/com/ocado/basket/error/InvalidConfigurationException.java
@@ -0,0 +1,7 @@
+package com.ocado.basket.error;
+
+public class InvalidConfigurationException extends Exception{
+    public InvalidConfigurationException(String message) {
+        super(message);
+    }
+}
